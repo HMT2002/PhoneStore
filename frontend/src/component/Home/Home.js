acfbc597d57038a1b81ae448c0a074ca35a7913f
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2023-07-06 00:00:45 +0700

    apply css for home + product detail page

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,12 +1,12 @@
-import React, { Fragment, useEffect } from 'react';
-import { CgMouse } from 'react-icons/cg';
-import './Home.css';
-import MetaData from '../layout/MetaData';
-import ProductCard from './ProductCard';
-import { getProduct, clearErrors } from '../../actions/productAction';
-import { useSelector, useDispatch } from 'react-redux';
-import Loader from '../layout/Loader/Loader';
-import { useAlert } from 'react-alert';
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/cg";
+import "./Home.css";
+import MetaData from "../layout/MetaData";
+import ProductCard from "./ProductCard";
+import { getProduct, clearErrors } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
 const Home = () => {
   const alert = useAlert();
@@ -24,21 +24,24 @@ const Home = () => {
         <Loader />
       ) : (
         <Fragment>
-          <MetaData title='MOBILE STORE' />
-          <div className='banner'>
-            <p className='text-3xl font-bold underline'>
-              CHÀO MỪNG QUÝ KHÁCH ĐÃ ĐẾN VỚI MOBILE STORE
+          <MetaData title="MOBILE STORE" />
+          <div className="banner">
+            <p className="text-3xl font-bold">
+              CHÀO MỪNG QUÝ KHÁCH ĐÃ ĐẾN VỚI
+              <span className="text-5xl block mt-3">MOBILE STORE</span>
             </p>
-            <h1>HÃY TÌM SẢN PHẨM YÊU THÍCH CỦA QUÝ KHÁCH Ở BÊN DƯỚI</h1>
+            <h1 className="mt-10">
+              HÃY TÌM SẢN PHẨM YÊU THÍCH CỦA QUÝ KHÁCH Ở BÊN DƯỚI
+            </h1>
 
-            <a href='#container'>
-              <button>
+            <a href="#container" className="mt-10">
+              <button className="bg-white px-4 py-3 flex items-center gap-2 text-gray-600 font-bold border-white border-2 hover:text-white hover:bg-transparent transition-all">
                 Sản phẩm <CgMouse />
               </button>
             </a>
           </div>
-          <h2 className='homeHeading'> Những sản phẩm của chúng tôi</h2>
-          <div className='container' id='container'>
+          <h2 className="homeHeading"> Những sản phẩm của chúng tôi</h2>
+          <div className="container" id="container">
             {products &&
               products.map((product) => <ProductCard product={product} />)}
           </div>
